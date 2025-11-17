73c34f6ccc671b7df57c45bd374b55d71098c5ea
nilesh-fatfatwale <fatfatwalenilesh@gmail.com>
2025-11-17 09:24:05 +0530

    adding jenkins shared files

---
+++ vars/docker_push.groovy
@@ -0,0 +1,8 @@
+def call(String DockerHubUser , String ProjectName ,String ImageTag ){
+withCredentials([usernamePassword(credentialsId: 'DockderHub',
+usernameVariable: 'dockerHubUser',
+passwordVariable: 'dockerHubPass' )]) {
+sh `docker login -u ${dockerHubUser} -p ${dockerHubPass}`
+}
+sh "docker push ${DockerHubUser}/${ProjectName}:${ImageTag}"
+}
